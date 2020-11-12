cfedd790a74c773105f67743cf02f9ec29bfee48
Steven Nguyen <steeven.nguyen1102@gmail.com>
2020-11-12 10:53:08 -0800

    finished seeding script mostly

--- server/index.js
+++ server/index.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const parser = require('body-parser');
+const db = require('../seeding.js');
 
 
 app.use(parser.json());
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get('/id', (req, res) => {
+    res.send('hello');
 })
 
 app.listen(port, () => {
