b1e3615c652fdc5d1b0d6e01db56ff9c9cdac028
Steven Nguyen <steeven.nguyen1102@gmail.com>
2020-11-27 19:44:33 -0800

    changed stylesheet reference to be in app.js

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import styles from '../public/styles.css';
 
 ReactDOM.render(<App />, document.getElementById('service4'));
