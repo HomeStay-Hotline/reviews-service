491e386b3547e7f239d3d3c0758da1302996cf62
Ying Guo <guoguoying2013@gmail.com>
2020-12-02 09:50:45 -0800

    Extend the existing API to support all CRUD operations within the inherited DBMS

--- server/database/index.js
+++ server/database/index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'huskies4405',
+    // password: null,
     database: 'reviews'
 });
 
--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ app.use(morgan('dev'));
 
 app.use('/:id', express.static('public'));
 
+// Read / GET 
 app.get('/api/homes/:id/reviews', (req, res) => {
   const { id } = req.params;
   const queryString = 'SELECT * FROM reviews WHERE reviews.listing_id = ?';
@@ -24,6 +25,60 @@ app.get('/api/homes/:id/reviews', (req, res) => {
     }
   });
 });
+
+// Create / POST - create a new review with an existing user
+app.post('/api/homes/:id/reviews', (req, res) => {
+  const { id } = req.params;
+  const body = req.body
+  console.log('body', body);
+  const queryString = 'INSERT INTO reviews SET ?';
+  body.listing_id = id;
+  body.review_date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+  const queryArgs = body;
+  console.log('queryArgs: ', queryArgs);
+  db.connection.query(queryString, queryArgs, (err, response) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(err);
+    } else {
+      res.send(response);
+    }
+  });
+})
+
+// Update / PUT - update an existing review
+app.put('/api/homes/:id/reviews', (req, res) => {
+  const queryArgs = req.body
+  // the id below is review id, not listing product id
+  const id = queryArgs.id;
+  delete queryArgs.id;
+  const queryString = 'UPDATE reviews SET ? WHERE id = ' + db.connection.escape(id);
+
+  db.connection.query(queryString, queryArgs, (err, response) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(err);
+    } else {
+      res.send(response);
+    }
+  });
+})
+
+
+// Delete / DELETE - delete a review
+app.delete('/api/homes/:id/reviews', (req, res) => {
+  const { id } = req.params;
+  const queryString = 'DELETE FROM reviews WHERE reviews.listing_id = ?';
+  const queryArgs = [id];
+  db.connection.query(queryString, queryArgs, (err, response) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(err);
+    } else {
+      res.send(response);
+    }
+  });
+})
 
 // app.use((req, res) => {
 //     res.end();
