a60acdf9a43f3afd924208462ab1792e465a9eed
Ying Guo <guoguoying2013@gmail.com>
2021-01-10 17:12:36 -0800

    cleaned up code in servicer index.js

--- server/index.js
+++ server/index.js
@@ -3,39 +3,48 @@ const express = require('express');
 
 const app = express();
 const port = 8080;
-const parser = require('body-parser');
-// const {Review, Customer} = require('./database/index.js');
-const {getAllInfo} = require('./database/index.js');
-const bodyparser = parser.json();
+
+const { getAllInfo } = require('./database/index.js');
 
 app.get(`/${process.env.VAR4}`, (req, res) => {
   res.send(`${process.env.VAR4}`);
-})
+});
 
 app.use('/:id', express.static('public'));
 
 app.get('/api/homes/:id/reviews', (req, res) => {
   getAllInfo(req.params.id, (err, data) => {
     if (err) {
-      console.log(err);
-      res.end();
+      res.end(err);
     }
     res.send(data);
   });
-  //////below is sequilizedxs
-  // const { id } = req.params;
-  // Review.findAll({
-  //   attributes: ['id', 'listing_id', 'customer_id', 'review', 'review_date', 'cleanliness', 'communication', 'check_in', 'accuracy', 'location_review', 'value_review'],
-  //   where: { listing_id : id },
-  //   include: [{model: Customer}],
-  // })
-  //   .then((data) => {
-  //     res.send(data);
-  //   })
-  //   .catch((err) => {
-  //     res.send(err);
-  //   })
 });
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+// Below is Sequelize version
+// const parser = require('body-parser');
+// const {Review, Customer} = require('./database/index.js');
+// const bodyparser = parser.json();
+// app.get('/api/homes/:id/reviews', (req, res) => {
+/// ///below is sequilizedxs
+// const { id } = req.params;
+// Review.findAll({
+// attributes: ['id', 'listing_id', 'customer_id', 'review', 'review_date', 'cleanliness', \
+// 'communication', 'check_in', 'accuracy', 'location_review', 'value_review'],
+//   where: { listing_id : id },
+//   include: [{model: Customer}],
+// })
+//   .then((data) => {
+//     res.send(data);
+//   })
+//   .catch((err) => {
+//     res.send(err);
+//   })
+// });
 
 // app.post('/api/homes/:id/reviews', bodyparser, (req, res) => {
 //   const body = req.body
@@ -48,7 +57,3 @@ app.get('/api/homes/:id/reviews', (req, res) => {
 //     res.send(err);
 //   })
 // })
-
-app.listen(port, () => {
-  console.log('Listening on port 8080');
-});
